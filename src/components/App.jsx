6828140d91a895fe59def2c0b775cb9e4fa587a9
Oleksandr <115025828+Oleksandr-Tsimbaliuk@users.noreply.github.com>
2023-06-23 18:59:54 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,14 +2,19 @@ import { useEffect, useState } from 'react';
 import Form from './Form';
 import ContactsList from './ContactsList';
 import Filter from './Filter';
+import { useDispatch, useSelector } from 'react-redux';
+import { addContact } from 'redux/contactSlice/contactSlice';
 
 export default function App() {
-  const [contacts, setContacts] = useState([
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ]);
+  const dispatch = useDispatch(state => state.appContacts.contacts);
+  const contacts = useSelector();
+
+  // const [contacts, setContacts] = useState([
+  //   { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  //   { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  //   { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  //   { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  // ]);
   const [filter, setFilter] = useState('');
 
   useEffect(() => {
@@ -22,7 +27,8 @@ export default function App() {
       return;
     }
 
-    setContacts(prevContacts => [...prevContacts, contactData]);
+    dispatch(addContact(contactData));
+    // setContacts(prevContacts => [...prevContacts, contactData]);
   };
 
   const changeFilter = event => {
@@ -36,9 +42,9 @@ export default function App() {
   };
 
   const deleteContact = contactId => {
-    setContacts(prevContacts =>
-      prevContacts.filter(contact => contact.id !== contactId)
-    );
+    // setContacts(prevContacts =>
+    //   prevContacts.filter(contact => contact.id !== contactId)
+    // );
   };
 
   const value = filteredContacts();
